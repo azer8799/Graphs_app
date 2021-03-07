2e8b2ff1b30c6a186b7866433b005e666a8bfbb0
azer8799 <anuprajput273@gmail.com>
2021-03-07 11:48:41 +0530

    graphs

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Login from './src/screens/Login.js';
+import  Info from './src/screens/Info.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+
+
+  const appNavigator = createStackNavigator(
+    {
+      HomePage: Login,
+      Information: Info,
+    },
+    
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(appNavigator);
---
+++ src/screens/Info.js
@@ -0,0 +1,209 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
+
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  
+} from 'react-native-chart-kit';
+import styles from '../styles/style.js';
+
+
+
+
+  const MyProgressChart = () => {
+    return (
+      <>
+        <Text style={styles.header}>Progress Chart</Text>
+        <ProgressChart
+          data={[0.4, 0.6, 0.8]}
+          width={Dimensions.get('window').width - 16}
+          height={220}
+          chartConfig={{
+            backgroundColor: '#1cc910',
+            backgroundGradientFrom: '#eff3ff',
+            backgroundGradientTo: '#efefef',
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </>
+    );
+  };
+
+  const MyLineChart = () => {
+    return (
+      <>
+        <Text style={styles.header}>Line Chart</Text>
+        <LineChart
+          data={{
+            labels: 
+              ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [20, 45, 28, 80, 99, 43],
+                strokeWidth: 2,
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width - 16}
+          height={220}
+          chartConfig={{
+            backgroundColor: '#1cc910',
+            backgroundGradientFrom: '#eff3ff',
+            backgroundGradientTo: '#efefef',
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </>
+    );
+  };
+
+  const MyBarChart = () => {
+    return (
+      <>
+        <Text style={styles.header}>Bar Chart</Text>
+        <BarChart
+          data={{
+            labels:
+              ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [20, 45, 28, 80, 99, 43],
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width - 16}
+          height={220}
+          yAxisLabel={'Rs'}
+          chartConfig={{
+            backgroundColor: '#1cc910',
+            backgroundGradientFrom: '#eff3ff',
+            backgroundGradientTo: '#efefef',
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </>
+    );
+  };
+
+  
+const MyPieChart = () => {
+  return (
+    <>
+      <Text style={styles.header}>Pie Chart</Text>
+      <PieChart
+        data={[
+          {
+            name: 'Seoul',
+            population: 21500000,
+            color: 'rgba(131, 167, 234, 1)',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Toronto',
+            population: 2800000,
+            color: '#F00',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'New York',
+            population: 8538000,
+            color: '#ffffff',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Moscow',
+            population: 11920000,
+            color: 'rgb(0, 0, 255)',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+        ]}
+        width={Dimensions.get('window').width - 16}
+        height={220}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute //For the absolute number else percentage
+      />
+    </>
+  );
+};
+    
+  const Info = () => {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView>
+          <View style={styles.container}>
+            <View>
+             
+              
+              <MyLineChart /> 
+              
+              <MyProgressChart />
+              
+              <MyBarChart /> 
+              
+             
+              
+              <MyPieChart />
+            
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
+  
+ 
+export default Info;
